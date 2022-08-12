9136acf55b0298f9f048eed78b5fade6e23c6b25
Max <realfalf@gmail.com>
2022-08-12 10:32:47 +0300

    stepic 2.1

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	// var text string
-	// fmt.Scanf("%s", &text)
-
-	reader := bufio.NewReader(os.Stdin)
-	text, _ := reader.ReadString('\n')
-
-	words := strings.Fields(text)
-	fmt.Println(len(words))
+	text := os.Args
+	fmt.Println(len(text[1:]))
 }
